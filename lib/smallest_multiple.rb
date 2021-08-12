574c7e00641d289514f7ab8e99ee5960a33a0687
Matthew Williams <love.cosma@gmail.com>
2021-08-11 21:23:16 -0600

    Done.

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,30 +1,21 @@
-# Implement your procedural solution here!
-def smallest_multiple(input)
+def smallest_multiple input
     range = (1..input).to_a
-
     multiple = 1
-    
+
     range.each do |num|
         multiple = lcm(multiple, num)
     end 
-    
-    multiple
-end 
+end
 
+def lcm 
+    a * b / gcd(a, b)
+end 
 
 def gcd(a, b)
     until b == 0
         temp = b
-        b  = a % b
+        b = b % a
         a = temp
     end 
-    return a
-end 
-
-def lcm(a, b)
-    return (a * b / gcd(a, b));
-end 
-
-def lcmm(max)
-   
+    a
 end 
